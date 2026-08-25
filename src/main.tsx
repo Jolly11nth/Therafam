f61e5ea829e674e058701bfaa77de0bd2cba73dc
Jolly Christopher <jollychris131@gmail.com>
2026-08-25 13:43:12 +0100

    Route React entry point through the new Therafam application shell

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import TherafamApp from './TherafamApp';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <TherafamApp />
   </React.StrictMode>
 );
