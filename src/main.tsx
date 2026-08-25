d2d1e832aecd95c62a3a9fa95915d9b2212ece44
Jolly Christopher <jollychris131@gmail.com>
2026-08-25 14:27:25 +0100

    Load visual polish and button interaction styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import TherafamApp from './TherafamApp';
 import './styles.css';
+import './theme-polish.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
